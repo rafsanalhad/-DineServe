33b86013eee0b880ce1b34412f29e341512bc265
armand010 <andikaputrasamawa@gmail.com>
2024-10-11 10:14:15 +0700

    fixButtonCamera

--- lib/ButtonCamera.dart
+++ lib/ButtonCamera.dart
@@ -5,17 +5,39 @@ class ButtonCamera extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
-      decoration: const ShapeDecoration(
-        color: Colors.lightBlue,
-        shape: CircleBorder(),
-      ),
-      child: IconButton(
-        icon: const Icon(Icons.camera_alt),
-        color: Colors.white,
-        onPressed: () {
-          Navigator.pushNamed(context, '/camera');
-        },
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'), // Judul AppBar
+        ),
+        body: Center(
+          // Mengatur posisi ButtonCamera di tengah layar
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Mengatur posisi kolom di tengah
+            children: [
+              Ink(
+                decoration: const ShapeDecoration(
+                  color: Colors.lightBlue,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.camera_alt),
+                  color: Colors.white,
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/camera');
+                  },
+                ),
+              ),
+              const SizedBox(height: 8), // Jarak antara ikon dan teks
+              const Text(
+                'Take a Picture', // Teks di bawah ikon
+                style:
+                    TextStyle(fontSize: 16, color: Colors.black), // Gaya teks
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
