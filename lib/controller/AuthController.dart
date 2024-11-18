a273dc89a7e4e44bb7e14dcd7a05b150ce8de43e
rafsanalhad <92444864+rafsanalhad@users.noreply.github.com>
2024-11-18 23:08:10 +0700

    ReservationDetail

--- lib/controller/AuthController.dart
+++ lib/controller/AuthController.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
+  var id = 0.obs; // id akan disimpan sebagai observables
   var username = ''.obs; // email akan disimpan sebagai observables
   
   void setUsername(String newUsername) {
     username.value = newUsername;
+  }
+  void setId(int newId) {
+    id.value = newId;
   }
 }
--- lib/views/History.dart
+++ lib/views/History.dart
@@ -1,135 +1,134 @@
-import 'package:dineserve/controller/reservation_provider.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
-import '../controller/reservation_controller.dart';
+import '../controller/AuthController.dart';
 
-// Import kelas ListReservasi yang sudah ada
-import '../models/ListReservasi.dart';
+class History extends StatefulWidget {
+  const History({super.key});
 
-class History extends StatelessWidget {
-  final ReservationController reservationController =
-      Get.find<ReservationController>();
-  final RxInt _selectedIndex = 0.obs;
+  @override
+  _HistoryState createState() => _HistoryState();
+}
+
+class _HistoryState extends State<History> {
+  late Future<List<dynamic>> _reservations;
+  final AuthController _authController = Get.find();
+
+  @override
+  void initState() {
+    super.initState();
+    _reservations = fetchReservations();
+  }
+
+  Future<List<dynamic>> fetchReservations() async {
+    final response = await http.get(
+      Uri.parse('http://localhost:5000/reservations?user_id=${_authController.id.value}'),
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> reservations = json.decode(response.body);
+      return reservations;
+    } else {
+      throw Exception('Failed to load reservations');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFF7F7F7),
+        backgroundColor: const Color(0xFFF7F7F7),
         title: const Text('History'),
       ),
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Color(0xFFF7F7F7),
-          ),
-          child: Obx(
-            () => ListView.builder(
-              itemCount: reservationController.reservations.length,
-              itemBuilder: (context, index) {
-                final reservasi = reservationController.reservations[index];
-
-                return Container(
-                  margin:
-                      EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 4),
-                      ),
-                    ],
-                  ),
-                  padding: EdgeInsets.all(16),
-                  child: Row(
-                    children: [
-                      Image.asset('assets/images/profil.png',
-                          width: 40, height: 40),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              reservasi.nama,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                              softWrap: true,
-                              overflow: TextOverflow.visible,
-                            ),
-                            Row(
-                              children: [
-                                Text('${reservasi.jam}, ',
-                                    style: TextStyle(
-                                        color: Colors.grey, fontSize: 12)),
-                                Text(reservasi.tgl,
-                                    style: TextStyle(
-                                        color: Colors.grey, fontSize: 12)),
-                              ],
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              reservasi.status,
-                              style: TextStyle(
-                                color: reservasi.status == 'Confirmed'
-                                    ? Colors.green
-                                    : reservasi.status == 'Pending'
-                                        ? Colors.orange
-                                        : Colors.red,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
+        child: FutureBuilder<List<dynamic>>(
+          future: _reservations,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text(snapshot.error.toString()));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No reservations found'));
+            } else {
+              final reservations = snapshot.data!;
+              return ListView.builder(
+                itemCount: reservations.length,
+                itemBuilder: (context, index) {
+                  final reservasi = reservations[index];
+                  return Container(
+                    margin: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: const Offset(0, 4),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: Obx(
-        () => BottomNavigationBar(
-          currentIndex: _selectedIndex.value,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'History',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'Profil',
-            ),
-          ],
-          selectedItemColor: const Color(0xFF18654A),
-          unselectedItemColor: const Color(0xFF18654A),
-          onTap: (index) {
-            _selectedIndex.value = index;
-            switch (index) {
-              case 0:
-                Get.toNamed('/home');
-                break;
-              case 1:
-                Get.toNamed('/history');
-                break;
-              case 2:
-                Get.toNamed('/profile');
-                break;
+                      ],
+                    ),
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      children: [
+                        const Image(
+                          image: AssetImage('assets/images/profil.png'),
+                          width: 40,
+                          height: 40,
+                        ),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                reservasi['details'] ?? 'No details available', // Handle null for 'details'
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    '${reservasi['time'] ?? 'No time available'}, ', // Handle null for 'time'
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    reservasi['date'] ?? 'No date available', // Handle null for 'date'
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: 4),
+                              Text(
+                                reservasi['status'] ?? 'Unknown status', // Handle null for 'status'
+                                style: TextStyle(
+                                  color: reservasi['status'] == 'Confirmed'
+                                      ? Colors.green
+                                      : reservasi['status'] == 'Pending'
+                                          ? Colors.orange
+                                          : Colors.red,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
             }
           },
         ),
@@ -137,154 +136,3 @@ class History extends StatelessWidget {
     );
   }
 }
-
-
-// class History extends StatefulWidget {
-//   const History({super.key});
-
-//   @override
-//   HistoryState createState() => HistoryState();
-// }
-
-// class HistoryState extends State<History> {
-//   int _selectedIndex = 0;
-
-//   final List<Widget> _pages = [
-//     const Center(child: Text('Home Page')),
-//     const Center(child: Text('History Page')),
-//     const Center(child: Text('Ulasan Page')),
-//   ];
-
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-//     switch (index) {
-//       case 0:
-//         Navigator.pushNamed(context, '/home');
-//         break;
-//       case 1:
-//         Navigator.pushNamed(context, '/history');
-//         break;
-//       case 2:
-//         Navigator.pushNamed(context, '/profile');
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Color(0xFFF7F7F7),
-//         title: const Text('History'),
-//       ),
-//       body: SafeArea(
-//         child: Container(
-//           decoration: BoxDecoration(
-//             color: Color(0xFFF7F7F7),
-//           ),
-//           child: Consumer<ReservationProvider>(
-//             builder: (context, reservationProvider, child) {
-//               final daftarReservasi = reservationProvider.reservations;
-
-//               return ListView.builder(
-//                 itemCount: daftarReservasi.length,
-//                 itemBuilder: (context, index) {
-//                   final reservasi = daftarReservasi[index];
-
-//                   return Container(
-//                     margin:
-//                         EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
-//                     decoration: BoxDecoration(
-//                       color: Colors.white,
-//                       borderRadius: BorderRadius.circular(8),
-//                       boxShadow: [
-//                         BoxShadow(
-//                           color: Colors.black.withOpacity(0.1),
-//                           spreadRadius: 1,
-//                           blurRadius: 5,
-//                           offset: Offset(0, 4),
-//                         ),
-//                       ],
-//                     ),
-//                     padding: EdgeInsets.all(16),
-//                     child: Row(
-//                       children: [
-//                         Image.asset('assets/images/profil.png',
-//                             width: 40, height: 40),
-//                         SizedBox(width: 10),
-//                         Expanded(
-//                           child: Column(
-//                             crossAxisAlignment: CrossAxisAlignment.start,
-//                             children: [
-//                               Text(
-//                                 reservasi.nama,
-//                                 style: TextStyle(
-//                                   fontSize: 16,
-//                                   fontWeight: FontWeight.bold,
-//                                 ),
-//                                 softWrap: true,
-//                                 overflow: TextOverflow.visible,
-//                               ),
-//                               Row(
-//                                 children: [
-//                                   Text('${reservasi.jam}, ',
-//                                       style: TextStyle(
-//                                           color: Colors.grey, fontSize: 12)),
-//                                   Text(reservasi.tgl,
-//                                       style: TextStyle(
-//                                           color: Colors.grey, fontSize: 12)),
-//                                 ],
-//                               ),
-//                               SizedBox(height: 4),
-//                               Text(
-//                                 reservasi.status,
-//                                 style: TextStyle(
-//                                   color: reservasi.status == 'Confirmed'
-//                                       ? Colors.green
-//                                       : reservasi.status == 'Pending'
-//                                           ? Colors.orange
-//                                           : Colors.red,
-//                                   fontSize: 12,
-//                                 ),
-//                               ),
-//                             ],
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-//                   );
-//                 },
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         currentIndex: _selectedIndex,
-//         showSelectedLabels: true,
-//         showUnselectedLabels: true,
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.history),
-//             label: 'History',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.person),
-//             label: 'Profil',
-//           ),
-//         ],
-//         selectedItemColor: const Color(0xFF18654A),
-//         unselectedItemColor: const Color(0xFF18654A),
-//         onTap: _onItemTapped,
-//       ),
-//     );
-//   }
-// }
