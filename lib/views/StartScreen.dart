00493565a78f49a156d51451e15a0ab72f559434
yoe174 <yoechannelpjr@gmail.com>
2024-12-18 02:03:38 +0700

    update ui

--- lib/views/StartScreen.dart
+++ lib/views/StartScreen.dart
@@ -1,7 +1,127 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
+// class StartScreen extends StatelessWidget {
+//   const StartScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//         child: Container(
+//           width: double.infinity,
+//           height: double.infinity,
+//           decoration: const BoxDecoration(
+//             gradient: LinearGradient(
+//               begin: Alignment.topCenter,
+//               end: Alignment.bottomCenter,
+//               colors: [Color(0xFF18654A), Color(0xFF30CB95)],
+//             ),
+//           ),
+//           child: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center, 
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: [
+//                 const Text(
+//                   'Mulai Reservasi Sekarang Dengan Mudah',
+//                   textAlign: TextAlign.center, 
+//                   style: TextStyle(
+//                     fontSize: 24,
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 10), 
+//                 const Text(
+//                   'Lakukan Reservasi Dengan Pengenalan Wajah dengan cepat dan mudah',
+//                   textAlign: TextAlign.center, 
+//                   style: TextStyle(
+//                     fontSize: 14,
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.w100,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 70), 
+//                 Container(
+//                   margin: const EdgeInsets.only(right: 40),
+//                   child: Image.asset(
+//                     'assets/images/hero_img.png',
+//                     width: 320,
+//                     height: 300,
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+              
+//                 Container(
+//                   width: MediaQuery.of(context).size.width * 0.8,
+//                   height: 50,
+//                   child: ElevatedButton(
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.white,
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(30),
+//                       ),
+//                     ),
+//                     onPressed: () {
+//                       Navigator.pushNamed(context, '/login');
+//                     },
+//                     child: const Text('Mulai Sekarang', style: TextStyle(color: Color(0xFF18654A))),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' as dot_env;
-class StartScreen extends StatelessWidget {
+import 'package:flutter/animation.dart';
+
+class StartScreen extends StatefulWidget {
   const StartScreen({super.key});
+
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+    ));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,54 +139,68 @@ class StartScreen extends StatelessWidget {
           ),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, 
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Text(
-                  'Mulai Reservasi Sekarang Dengan Mudah',
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: const Text(
+                    'Mulai Reservasi Sekarang Dengan Mudah',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10), 
-                const Text(
-                  'Lakukan Reservasi Dengan Pengenalan Wajah dengan cepat dan mudah',
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w100,
+                const SizedBox(height: 10),
+                FadeTransition(
+                  opacity: _fadeAnimation,
+                  child: const Text(
+                    'Lakukan Reservasi Dengan Pengenalan Wajah dengan cepat dan mudah',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w100,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 70), 
-                Container(
-                  margin: const EdgeInsets.only(right: 40),
-                  child: Image.asset(
-                    'assets/images/hero_img.png',
-                    width: 320,
-                    height: 300,
-                    fit: BoxFit.cover,
+                const SizedBox(height: 70),
+                SlideTransition(
+                  position: _slideAnimation,
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 40),
+                    child: Image.asset(
+                      'assets/images/hero_img.png',
+                      width: 320,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 20),
-              
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.8,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                SlideTransition(
+                  position: _slideAnimation,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      child: const Text(
+                        'Mulai Sekarang',
+                        style: TextStyle(color: Color(0xFF18654A)),
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: const Text('Mulai Sekarang', style: TextStyle(color: Color(0xFF18654A))),
                   ),
                 ),
               ],
@@ -77,3 +211,4 @@ class StartScreen extends StatelessWidget {
     );
   }
 }
+
